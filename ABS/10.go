d7f71136b776d4f33fdae9ea38c2f238e65da26b
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2022-01-19 22:58:02 +0900

    10

--- ABS/10.go
+++ ABS/10.go
@@ -1,1 +1,34 @@
-// https://atcoder.jp/contests/abs/tasks/abc085_c
+// https://atcoder.jp/contests/abs/tasks/arc065_a
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	var s string
+	li := []string{"dreamer", "dream", "erase", "eraser"}
+	fmt.Fscan(r, &s)
+	tmp := s
+	for {
+		for _, tm := range li {
+			s = strings.TrimSuffix(s, tm)
+		}
+		if s == "" {
+			fmt.Fprintln(w, "YES")
+			break
+		}
+		if s == tmp {
+			fmt.Fprintln(w, "NO")
+			break
+		}
+		tmp = s
+	}
+}
