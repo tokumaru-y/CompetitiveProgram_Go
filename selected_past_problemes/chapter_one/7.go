a8a432cd4b1a1b46417cfe309aadeaf5eec32713
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2022-01-25 22:23:54 +0900

    7

---
+++ selected_past_problemes/chapter_one/7.go
@@ -0,0 +1,57 @@
+// https://www.ioi-jp.org/joi/2006/2007-ho-prob_and_sol/2007-ho.pdf#page=5
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Cordinate struct {
+	xs int
+	ys int
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	var n int
+	fmt.Fscan(r, &n)
+	X := make([]int, n)
+	Y := make([]int, n)
+	m := map[Cordinate]int{}
+	for i := 0; i < n; i++ {
+		var x, y int
+		fmt.Fscan(r, &x)
+		fmt.Fscan(r, &y)
+		X[i] = x
+		Y[i] = y
+		m[Cordinate{xs: x, ys: y}] = 1
+	}
+	var ans int
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			lx := X[i] - X[j]
+			ly := Y[i] - Y[j]
+			rx := X[j] - X[i]
+			ry := Y[j] - Y[i]
+			if _, ok := m[Cordinate{xs: -lx, ys: ry}]; ok {
+				if _, ok := m[Cordinate{xs: rx, ys: -ly}]; ok {
+					v := lx*lx + ly*ly
+					if ans < v {
+						ans = v
+					}
+				}
+			}
+		}
+	}
+	fmt.Fprintln(w, ans)
+}
